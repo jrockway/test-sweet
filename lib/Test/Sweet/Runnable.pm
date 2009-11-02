37aecdf8dc9e0ee4f94d67df972dacc879144815
Jonathan Rockway <jon@jrock.us>
2009-11-02 02:36:45 -0600

    optionally use mx-getopt if it is installed

--- lib/Test/Sweet/Runnable.pm
+++ lib/Test/Sweet/Runnable.pm
@@ -3,6 +3,11 @@ use MooseX::Declare;
 role Test::Sweet::Runnable with MooseX::Runnable {
     use Test::More;
 
+    eval {
+        require MooseX::Getopt;
+        with 'MooseX::Getopt';
+    };
+
     method run {
         my @tests = $self->meta->get_all_tests;
         plan tests => scalar @tests; # so you get a "progress bar"
